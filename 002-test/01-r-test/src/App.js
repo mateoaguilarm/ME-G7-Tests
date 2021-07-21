d3d9a23397e234399ec168a5a9649e51e31bc89d
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-07-20 21:26:22 -0500

    info de 20/07

--- 002-test/01-r-test/src/App.js
+++ 002-test/01-r-test/src/App.js
@@ -11,8 +11,9 @@ function App() {
     <div className="App">
       <h1>Message from App.js 👨‍💻 </h1>
       <FuncComp />
-      <Card name='asd' adress='asd2' number='asd3' email='asd4' />
-      <Card name='asd' adress='asd2' number='asd3' email='asd4' color='white'/>
+      <Card name='Sebastian' adress='Casa032' number='12345' email='sebas@test.com' />
+      <Card name='Rick' adress='Casa034' number='43199' email='rick@test.com' color='white'/>
+      <Card name='Bill' adress='Casa941' number='87439' email='bill@test.com' color='yellow'/>
     </div>
   );
 }
