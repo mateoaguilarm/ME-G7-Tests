b5c8c282e0cb0b0170225d3c9c81096a049afdf4
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-07-21 20:04:04 -0500

    clase Hooks 21/07

---
+++ 002-test/03-r-test/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Example from './components/Example';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Messaje from App.js 📲</h1>
+      <Example />
+    </div>
+  );
+}
+
+export default App;
