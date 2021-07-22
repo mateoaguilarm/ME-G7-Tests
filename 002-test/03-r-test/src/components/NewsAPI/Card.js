0e15c31acbc3294001d8cc19e6b420fb2a65294d
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-07-21 21:50:55 -0500

    clase de Hooks con API completada

--- 002-test/03-r-test/src/components/NewsAPI/Card.js
+++ 002-test/03-r-test/src/components/NewsAPI/Card.js
@@ -2,18 +2,19 @@ import React, { useState } from 'react'
 import '../NewsAPI/Card.css'
 
 function Card(props) {
+    const { title, desc, img, color} = props;
 
-    const imgURL = 'https://cocina-casera.com/wp-content/uploads/2018/03/mochi.jpg'
+    // const imgURL = 'https://cocina-casera.com/wp-content/uploads/2018/03/mochi.jpg'
 
     const [ likes, setLikes ] = useState(0);
     
     const handleLikes = () => setLikes(likes + 1)
 
     return (
-        <div className='newsCard' style={{ backgroundColor: props.color }}>
-            <img className='imgCard' src={imgURL}/>
-            <h4 className='titleCard'> {props.title} </h4>
-            <p className='descCard'> {props.desc} </p>
+        <div className='newsCard' style={{ backgroundColor: color }}>
+            <img className='imgCard' src={img} alt='image'/>
+            <h4 className='titleCard'> {title} </h4>
+            <p className='descCard'> {desc} </p>
             <a className='a-Card' href='/Mochis'> See more </a>
             <button className='likeBtn' type="submit" onClick={handleLikes}>Like👍</button>
             <span>Likes:{likes}</span>
--- 002-test/03-r-test/src/components/NewsAPI/News.js
+++ 002-test/03-r-test/src/components/NewsAPI/News.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+import Card from '../NewsAPI/Card'
 
 function News(props) {
+
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+        const apiURL = 'http://www.rtve.es/api/noticias.json'
+        axios.get(apiURL)
+            .then(response => {
+                setNews(response.data.page.items);
+                console.log(response.data.page.items);
+            })
+            .catch(error => console.log(error))
+    }, []);
+
+    const renderNews = () => {
+        if (news.length === 0) {
+            return <h2>Cargando noticias...</h2>
+        } else if (news.length > 0) {
+            return (
+                <>
+                    <h2> Se encontraron {news.length} noticias </h2>
+                    <div>
+                        {
+                            news.map(noticia => {
+                                return <Card title={noticia.title} desc={noticia.longTitle} img={noticia.image} />
+                            })
+                        }
+                    </div>
+                </>
+            );
+        }
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+            {
+                renderNews()
+            }
+        </>
     )
 }
 
