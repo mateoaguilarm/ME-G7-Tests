bc6148428d229c5eb177c78d654f0a89f267a0b1
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-08-05 21:06:42 -0500

    App Shows First Result in API

--- 003-test/01-repaso/app.js
+++ 003-test/01-repaso/app.js
@@ -1,1 +1,43 @@
 console.log('hola en app');
+
+// 1. Bring the ID's from the HTML
+const formAlbums = document.querySelector('#formAlbums');
+const cardAlbums = document.querySelector('#albumCard');
+
+const API_URL = 'https://crudcrud.com/api/39bc61cdd1fa4061839d2dd5fff7f0c0/albums';
+
+const getAlbums = async () => {
+    cardAlbums.innerHTML = '';
+
+    try {
+        const response = await fetch(API_URL);
+        const albums = await response.json();
+
+        cardAlbums.innerHTML = renderAlbums(albums)
+    } catch (err) {
+        throw new Error('Error: ', err)
+    }
+
+}
+
+const renderAlbums = (albums) => {
+    return albums.map(album => {
+        const { albumName, year, songs, coverURL } = album;
+        const albumCardHTML =
+            `<div id="albumCard" class="row row-cols1 row-cols-md-2 g-4 m-5">
+        <div class="col">
+            <div  class="card m-5" style="width: 18rem;">
+                <img src=" ${coverURL} " class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title text-center pb-4">${albumName}</h5>
+                    <p class="card-text lead"> Release Date: ${year} </p>
+                    <p class="card-text lead"> Songs: ${songs} </p>
+                </div>
+            </div>
+        </div>
+    </div>`
+        return albumCardHTML;
+    })
+}
+
+getAlbums()
