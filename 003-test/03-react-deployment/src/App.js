c0ace9f0a56d6290d0aced8bb3e4af857f93cf14
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-08-16 19:48:20 -0500

    test ready to deploy

--- 003-test/03-react-deployment/src/App.js
+++ 003-test/03-react-deployment/src/App.js
@@ -1,10 +1,13 @@
 
 // import './App.css';
 
+import PostCard from './PostCard'
+
 function App() {
   return (
     <div className="App">
       <h1>hola en app 💻</h1>
+      <PostCard />
     </div>
   );
 }
---
+++ 003-test/03-react-deployment/src/PostCard.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import './PostCard.css'
+
+function PostCard() {
+
+    const Lorem = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil eligendi tenetur modi voluptatibus amet dolores ea, atque suscipit perferendis nam in rem adipisci ducimus ratione veniam sed, dolore iusto aliquid?'
+
+    const [count, setCount] = useState(0)
+    const Increase = () => {
+        setCount( count + 1 )
+    } 
+    const Decrease = () => {
+        setCount( count - 1 )
+    } 
+
+    return (
+        <div className='bodyCard'>
+            <h3 className='tCard'>Title</h3>
+            <p className='descCard'> {Lorem} </p>
+            <div>
+                <button onClick={Decrease} className='btnsCard'>👎</button>
+                <span> {count} </span>
+                <button onClick={Increase} className='btnsCard'>👍</button>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default PostCard
