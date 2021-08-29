3b69bacec1ded5c223cfd52a7cae62c2af1565b5
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-08-29 18:51:42 -0500

    installing bootstrap, setting 1st component

--- 004-test/01-components-test/src/App.js
+++ 004-test/01-components-test/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import NavbarMain from './components/NavbarMain';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hola en App 💻</h1>
+      <NavbarMain />
     </div>
   );
 }
---
+++ 004-test/01-components-test/src/components/NavbarMain.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+
+function NavbarMain() {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Salud Aguilar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Inicio</Nav.Link>
+                        <Nav.Link href="#features">Quienes Somos</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default NavbarMain
