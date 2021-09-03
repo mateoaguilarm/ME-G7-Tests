685b3eaa390f3e05a936724e18896f188c443b97
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-09-02 22:10:42 -0500

    setting up firebase 1/2

--- 004-test/01-components-test/src/App.js
+++ 004-test/01-components-test/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
+import firebaseApp from './firebase';
+
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 import Footer from './components/footer/Footer';
 import NavbarMain from "./components/NavbarMain";
 import Home from './views/Home'
 import Services from './views/Services'
+
+console.log(firebaseApp, 'log from App.js');
 
 function App() {
   return (
