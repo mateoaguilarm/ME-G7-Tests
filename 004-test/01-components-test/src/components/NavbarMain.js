22fbcf66914d9b78ce10a7b5f1b8005bb1fde5be
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-09-02 22:11:04 -0500

    small issue

--- 004-test/01-components-test/src/components/NavbarMain.js
+++ 004-test/01-components-test/src/components/NavbarMain.js
@@ -1,6 +1,6 @@
 import React, { } from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 // import '../index.css'
 
 function NavbarMain() {
@@ -33,13 +33,6 @@ function NavbarMain() {
                             <Nav.Link>Login</Nav.Link>
                         </LinkContainer>
 
-                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown> */}
                     </Nav>
                 </Container>
             </Navbar>
