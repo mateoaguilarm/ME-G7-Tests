c6911ef6abf57c693f7e326153bd5158d7bd11e2
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-09-15 17:08:49 -0500

    limpieza de App.js, dejando para probar el Sign Up

--- 004-test/01-components-test/src/App.js
+++ 004-test/01-components-test/src/App.js
@@ -1,30 +1,36 @@
 import React from "react";
-import firebaseApp from './firebase';
+// import firebaseApp from './firebase';
+import { AuthProvider } from './contexts/authContext'
 
 import {
   BrowserRouter as Router,
-  Switch,
-  Route
+  // Switch,
+  // Route
 } from "react-router-dom";
 
-import Footer from './components/footer/Footer';
-import NavbarMain from "./components/NavbarMain";
-import Home from './views/Home'
-import Services from './views/Services'
+// import Footer from './components/footer/Footer';
+// import NavbarMain from "./components/NavbarMain";
+// import Home from './views/Home'
+// import Services from './views/Services'
+import SignUp from "./components/SignUp";
 
-console.log(firebaseApp, 'log from App.js');
+// console.log(firebaseApp, 'log from App.js');
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <NavbarMain />
-        <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route path='/services' component={Services}/>
-        </Switch>
-        <Footer />
-      </div>
+      <AuthProvider>
+        <SignUp />
+        {/* <div className="App">
+          <NavbarMain />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/services' component={Services} />
+          </Switch>
+          <Footer />
+        </div> */}
+      </AuthProvider>
+
     </Router>
 
   );
