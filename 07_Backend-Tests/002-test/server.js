52fb159e59356aa98d9c03f2a3a254ebe00e8f19
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-11-18 23:09:58 -0500

    Step 3 (base part)

---
+++ 07_Backend-Tests/002-test/server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end('MSG to body');
+    console.log(
+        `La ruta de la petición es: ${request.url} con el método ${request.method}`
+    );
+});
+
+server.listen(port, hostname, () => {
+    console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+});
