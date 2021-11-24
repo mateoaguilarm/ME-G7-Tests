48678e19d1289bb7d1305155e286d86daf2c8061
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-11-24 15:59:37 -0500

    express base server, 2nd step

--- 07_Backend-Tests/003-test/server.js
+++ 07_Backend-Tests/003-test/server.js
@@ -7,3 +7,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+// MIDDLEWARES
+    // Second step is to configure the middleware
+
+app.use(express.json()); // This middleware allows the .json files on server.
+app.use(express.urlencoded({ extended: true })); // Allows the format application/x-www-form-urlencoded (docs, media, etc)
