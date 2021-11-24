ec760e37586cea5ac8023479c393e550c32ce57a
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-11-24 15:54:34 -0500

    express server 1st step

---
+++ 07_Backend-Tests/003-test/server.js
@@ -0,0 +1,9 @@
+// expressJS Base Server
+
+// MODULES
+    // Once express is installed, first step is to require the dependencie.
+    // And declare as an app, then the port.
+
+const express = require('express');
+const app = express();
+const port = 3000;
